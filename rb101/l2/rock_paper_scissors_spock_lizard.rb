1c3ac3c9ea97279098a979079aceb95f82bf2825
Florence Shelley <smc.florence@gmail.com>
2023-03-05 17:26:58 -0500

    refactor: allow player to input full name or abbr

--- rb101/l2/rock_paper_scissors_spock_lizard.rb
+++ rb101/l2/rock_paper_scissors_spock_lizard.rb
@@ -24,8 +24,13 @@ def prompt(msg)
   puts("=> #{msg}")
 end
 
-def valid_choice?(choice)
-  VALID_CHOICES.values.include?(choice)
+def valid_choice?(player_choice)
+  name, abbr = VALID_CHOICES.find do |_, value|
+    player_choice.start_with?(value)
+  end
+
+  # check if the choice input is the full name or the abbreviation
+  name.to_s == player_choice || abbr == player_choice
 end
 
 def generate_computer_choice
@@ -34,14 +39,29 @@ end
 
 def interpolated_choices
   choices = []
+
   VALID_CHOICES.each { |key, value| choices << "#{value} (#{key})" }
 
   choices
 end
 
+# get the abbreviated name of the player's choice
+def choice_abbr(choice)
+  _, abbr = choice_pair(choice)
+  abbr
+end
+
+# get the full name of the player's choice
 def choice_name(choice)
-  choice = VALID_CHOICES.find { |_, value| value == choice }
-  choice[0].to_s
+  name, = choice_pair(choice)
+  name.to_s
+end
+
+# return the full name and abbr of the player's choice
+def choice_pair(choice)
+  VALID_CHOICES.find do |key, value|
+    key.to_s == choice || value == choice
+  end
 end
 
 def get_player_choice
@@ -58,9 +78,11 @@ def get_player_choice
 end
 
 def get_outcome(player_choice, computer_choice)
-  if WINNING_CHOICES[player_choice].include?(computer_choice)
+  choice = choice_abbr(player_choice)
+
+  if WINNING_CHOICES[choice].include?(computer_choice)
     OUTCOMES[:win]
-  elsif WINNING_CHOICES[computer_choice].include?(player_choice)
+  elsif WINNING_CHOICES[computer_choice].include?(choice)
     OUTCOMES[:lose]
   else
     OUTCOMES[:draw]
